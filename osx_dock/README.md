7c41e831639b91207f0081052d0bbad8fa28e73e
IceMupppet <adam+github@orbotix.com>
2012-02-04 15:14:03 -0700

    Adding osx_dock and misson_control themes

---
+++ osx_dock/README.md
@@ -0,0 +1,1 @@
+# Just go into the directory of the theme you want and ./install.sh
---
+++ osx_dock/black/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+sudo cp scurve-sm.png /System/Library/CoreServices/Dock.app/Contents/Resources/scurve-sm.png
+sudo cp scurve-m.png /System/Library/CoreServices/Dock.app/Contents/Resources/scurve-m.png
+sudo cp scurve-l.png /System/Library/CoreServices/Dock.app/Contents/Resources/scurve-l.png
+sudo cp scurve-xl.png /System/Library/CoreServices/Dock.app/Contents/Resources/scurve-xl.png
+
+
+
+sudo killall Dock
+
+
