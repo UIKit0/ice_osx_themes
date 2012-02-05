18c6716851c2705a9420c82cfec27996ce3caf31
IceMupppet <adam+github@orbotix.com>
2012-02-05 08:37:34 -0700

    Created a design directory for PSD files

---
+++ mission_control/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+sudo cp NSTexturedFullScreenBackgroundColor.png  /System/Library/CoreServices/Dock.app/Contents/Resources/NSTexturedFullScreenBackgroundColor.png
+
+echo "You must restart for the mission control change to take effect!"
+
+
